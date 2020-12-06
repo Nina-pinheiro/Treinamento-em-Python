e4fd616582536ab31b482b45ce2e422ac814890b
Nina-pinheiro <ninapinheiro@usp.br>
2020-12-06 20:38:56 -0300

    Atualizando mais exercicios de loop

--- Treinamentos/exercicio_repeticao.py
+++ Treinamentos/exercicio_repeticao.py
@@ -1,5 +1,5 @@
 '''Script que eu realizo exercícios para treinar lógica de programação
-Nome: Nina M.I.S.P.Machado
+Nome: Nina 
 
 Exercícios sobre Estrutura de Repetição
 
@@ -67,6 +67,63 @@ print("A soma dos 5 números é ", soma)
 print("A média dos 5 números é ", media)
 
 
+#6Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. 
+#Depois modifique o programa para que ele mostre os números um ao lado do outro.
+
+# Loop 
+for numero in range(1,21):
+  print(numero)
+ 
+print(list(range(1,21)))
+
+#Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50
+
+for num_impar in range (1,51,2):
+  print(num_impar)
+
+#7Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário. Ex.: 5!=5.4.3.2.1=120.
+# A saída deve ser conforme o exemplo abaixo
+
+numero = int(input("Digite um número"))
+lista=[]
+resultado =1
+for fatorial in range(1,numero+1):
+  resultado = resultado*fatorial
+  
+  
+print(resultado)
+
+#8Em uma competição de ginástica, cada atleta recebe votos de sete jurados. A melhor e a pior nota são eliminadas. 
+#A sua nota fica sendo a média dos votos restantes. Você deve fazer um programa que receba o nome do ginasta e as notas dos sete jurados alcançadas 
+#pelo atleta em sua apresentação e depois informe a sua média, conforme a descrição acima informada (retirar o melhor e o pior salto e 
+#depois calcular a média com as notas restantes). As notas não são informados ordenadas. 
 
 
+# Pedir o nome
+nome = str(input("Digite o seu nome: "))
+lista = []
 
+# Loop que vai pedir 7 vezes a nota
+for i in range(1,8):
+  nota = int(input(f'Digite a {i}° nota : '))
+  lista.append(nota)
+
+maior_nota = max(lista)
+menor_nota = min(lista)
+
+# Remove a maior nota
+lista.remove(maior_nota)
+# Remove a menor nota
+lista.remove(menor_nota)
+# Verifica o tamanho da lista
+quantidade = len(lista)
+# Faz a soma das notas
+soma_das_notas = sum(lista)
+# Faz a media das notas
+media = soma_das_notas/quantidade
+
+print(f'Resultado final:')
+print(f'Atleta: {nome}')
+print(f'Melhor nota: {maior_nota}')
+print(f'Pior nota: {menor_nota}')
+print(f'Média: {media}')
