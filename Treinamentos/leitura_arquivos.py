db5023bc5167334a2f4bbe4f1522e2f5ad1d9ef2
Nina-pinheiro <ninapinheiro@usp.br>
2020-12-21 14:16:31 -0300

    Leitura de arquivos atualizada

---
+++ Treinamentos/leitura_arquivos.py
@@ -0,0 +1,45 @@
+"""
+Treine conceitos básicos de leitura de arquivos
+
+Autora: Nina Pinheiro 
+Data
+"""
+
+
+# Abrindo o arquivo para leitura em formato csv
+
+segundo_arquivo = open("arquivos/arq2.csv","r")
+
+# Ler o arquivo para leitura em formato csv
+
+print(segundo_arquivo.read())
+
+# Contar a quantidade pelo metodo "tell"
+
+print(segundo_arquivo.tell())
+
+# Grava o arquivo no objeto data e consegue ler
+data = segundo_arquivo.read()
+
+# Dividir o arquivo com base em critério(no caso linha)usando o método "split"
+
+rows = data.split('\n')
+
+print(rows)
+
+# Outra forma de abrir o arquivo csv é por pandas
+
+import pandas as pd
+
+file_name = "arquivos/arq2.csv"
+
+# Aqui você consegue ler o arquivo pelo pandas
+df = pd.read_csv(file_name)
+
+# Consegue ler os primerios arquivos
+df.head()
+
+
+
+
+
